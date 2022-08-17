4a493f3f68819512a9ad82213fa0936f399df7c5
shivesh-pro <shiveshkarthic@gmail.com>
2022-08-17 12:10:49 +0530

    Cloud

---
+++ SpringCloud/src/test/java/com/org/DemoService/SpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.DemoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
