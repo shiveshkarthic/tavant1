51eda7827067f35255af69331ffd3ee165ffe59d
shivesh-pro <shiveshkarthic@gmail.com>
2022-08-17 09:46:03 +0530

    SpringBoot MVC1

---
+++ SpringBootMVC-example1/src/main/java/com/example/mvc/SpringBootMvcExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcExample1Application.class, args);
+	}
+
+}
